700193195d8c78e776ea1cfa93d90327b3b03a15
Rachit-Goel <rachitgoel99@gmail.com>
2022-02-22 17:47:34 +0530

    sign up, profile img, exp with prjts & url done

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-
-  resources :profiles , only: [:update]
+  get    'createacc'  => 'signup#new'
+  post    'createacc'  => 'signup#create'
+  # get "projects" => "projects#new"
+  # post "projects" => "projects#new"
+  resources :profiles , only: [:show,:update]
   resources :educations , only: [:new]
+  resources :experiences , only: [:new]
+  resources :projects, only: [:new]
 end
